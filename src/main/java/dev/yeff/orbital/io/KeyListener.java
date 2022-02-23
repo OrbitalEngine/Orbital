f8e515cf098f946880c5809681183211dc118519
YeffyCodeGit <aditchakraborty2009@gmail.com>
2022-02-23 21:36:40 +0600

    added docs for the rest of the code

--- src/main/java/dev/yeff/orbital/io/KeyListener.java
+++ src/main/java/dev/yeff/orbital/io/KeyListener.java
@@ -3,12 +3,23 @@ package dev.yeff.orbital.io;
 import static org.lwjgl.glfw.GLFW.GLFW_PRESS;
 import static org.lwjgl.glfw.GLFW.GLFW_RELEASE;
 
+/**
+ * Class storing keyboard input callbacks and functions to receive input from the keyboard.
+ *
+ * @author YeffyCodeGit
+ * @version 0.0.1
+ */
 public class KeyListener {
     private static KeyListener instance;
     private boolean keyPressed[] = new boolean[350];
 
     private KeyListener() {}
 
+    /**
+     * Returns the single instance of the class.
+     *
+     * @return The KeyListener instance.
+     */
     public static KeyListener getInstance() {
         if (KeyListener.instance == null)
             KeyListener.instance = new KeyListener();
@@ -16,6 +27,15 @@ public class KeyListener {
         return KeyListener.instance;
     }
 
+    /**
+     * Callback for OpenGL to get keyboard input.
+     *
+     * @param window The window pointer.
+     * @param key The key pressed.
+     * @param scancode The scan code.
+     * @param action The action, like press or release.
+     * @param mods The modifier keys.
+     */
     public static void keyCallback(long window, int key, int scancode, int action, int mods) {
         if (action == GLFW_PRESS)
             getInstance().keyPressed[key] = true;
@@ -23,6 +43,12 @@ public class KeyListener {
             getInstance().keyPressed[key] = false;
     }
 
+    /**
+     * Checks if a key on the keyboard is held down.
+     *
+     * @param keyCode The key to check.
+     * @return If the key is held down or not.
+     */
     public static boolean isKeyDown(int keyCode) {
         return getInstance().keyPressed[keyCode];
     }
--- src/main/java/dev/yeff/orbital/io/MouseListener.java
+++ src/main/java/dev/yeff/orbital/io/MouseListener.java
@@ -3,6 +3,12 @@ package dev.yeff.orbital.io;
 import static org.lwjgl.glfw.GLFW.GLFW_PRESS;
 import static org.lwjgl.glfw.GLFW.GLFW_RELEASE;
 
+/**
+ * Class storing mouse input callbacks and functions to receive input and other data from the mouse.
+ *
+ * @author YeffyCodeGit
+ * @version 0.0.1
+ */
 public class MouseListener {
     public static MouseListener instance;
     private double scrollX, scrollY;
@@ -19,6 +25,11 @@ public class MouseListener {
         this.lastY = 0.0;
     }
 
+    /**
+     * Returns the single instance of the class.
+     *
+     * @return The MouseListener instance.
+     */
     public static MouseListener getInstance() {
         if (MouseListener.instance == null)
             instance = new MouseListener();
@@ -26,6 +37,13 @@ public class MouseListener {
         return MouseListener.instance;
     }
 
+    /**
+     * Callback for OpenGL to get the mouse position.
+     *
+     * @param window The window pointer.
+     * @param xPos The x position of the mouse.
+     * @param yPos The y position of the mouse.
+     */
     public static void mousePosCallback(long window, double xPos, double yPos) {
         getInstance().lastX = getInstance().xPos;
         getInstance().lastY = getInstance().yPos;
@@ -38,6 +56,14 @@ public class MouseListener {
                             || getInstance().mouseButtonPressed[2];
     }
 
+    /**
+     * Callback for OpenGL to get if a mouse button was pressed.
+     *
+     * @param window The window pointer.
+     * @param button The mouse button.
+     * @param action The action, like press or release.
+     * @param mods The modifier keys.
+     */
     public static void mouseButtonCallback(long window, int button, int action, int mods) {
         if (action == GLFW_PRESS) {
             if (button < getInstance().mouseButtonPressed.length)
@@ -50,11 +76,21 @@ public class MouseListener {
         }
     }
 
+    /**
+     * Callback for OpenGL to get the mouse scroll position.
+     *
+     * @param window The window pointer.
+     * @param xOffset The x scroll position.
+     * @param yOffset The y scroll position.
+     */
     public static void mouseScrollCallback(long window, double xOffset, double yOffset) {
         getInstance().scrollX = xOffset;
         getInstance().scrollY = yOffset;
     }
 
+    /**
+     * Resets all the values.
+     */
     public static void endFrame() {
         getInstance().scrollX = 0;
         getInstance().scrollY = 0;
@@ -62,34 +98,75 @@ public class MouseListener {
         getInstance().lastY = getInstance().yPos;
     }
 
+    /**
+     * Gets the x position of the mouse.
+     *
+     * @return The x position of the mouse.
+     */
     public static float getX() {
         return (float) getInstance().xPos;
     }
 
+    /**
+     * Gets the y position of the mouse.
+     *
+     * @return The y position of the mouse.
+     */
     public static float getY() {
         return (float) getInstance().yPos;
     }
 
+    /**
+     * Gets the last x position of the mouse.
+     *
+     * @return The last x position of the mouse.
+     */
     public static float getDx() {
         return (float) (getInstance().lastX - getInstance().xPos);
     }
 
+    /**
+     * Gets the last y position of the mouse.
+     *
+     * @return The last y position of the mouse.
+     */
     public static float getDy() {
         return (float) (getInstance().lastY - getInstance().yPos);
     }
 
+    /**
+     * Gets the x scroll position of the mouse.
+     *
+     * @return The x scroll position of the mouse.
+     */
     public static float getScrollX() {
         return (float) getInstance().scrollX;
     }
 
+    /**
+     * Gets the y scroll position of the mouse.
+     *
+     * @return The y scroll position of the mouse.
+     */
     public static float getScrollY() {
         return (float) getInstance().scrollY;
     }
 
+    /**
+     * Gets if the mouse is dragging or not.
+     *
+     * @return If the mouse is dragging or not.
+     */
     public static boolean isDragging() {
         return getInstance().isDragging;
     }
 
+    /**
+     * Checks if a mouse button is held down.
+     *
+     * @param button The mouse button.
+     * @return If the mouse button is held down.
+     */
     public static boolean mouseButtonDown(int button) {
         if (button < getInstance().mouseButtonPressed.length)
             return getInstance().mouseButtonPressed[button];
