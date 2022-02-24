d7f83d0e8424daaf7c26941cfb012adad25ff3a4
YeffyCodeGit <aditchakraborty2009@gmail.com>
2022-02-24 15:37:06 +0600

    added documentation to renderer

--- src/main/java/dev/yeff/orbital/renderer/Renderer.java
+++ src/main/java/dev/yeff/orbital/renderer/Renderer.java
@@ -2,6 +2,12 @@ package dev.yeff.orbital.renderer;
 
 import static org.lwjgl.opengl.GL11.*;
 
+/**
+ * Class that handles all rendering.
+ *
+ * @author YeffyCodeGit
+ * @version 0.0.1
+ */
 public class Renderer {
     public void renderGameObject(GameObject object) {
         object.use();
