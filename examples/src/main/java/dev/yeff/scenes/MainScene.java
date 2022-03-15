569d0440763edeb9e740911b7675b76910aea03b
YeffyCodeGit <aditchakraborty2009@gmail.com>
2022-03-15 17:55:17 +0600

    create a proper example app

--- examples/src/main/java/dev/yeff/scenes/MainScene.java
+++ examples/src/main/java/dev/yeff/scenes/MainScene.java
@@ -1,6 +1,7 @@
 package dev.yeff.scenes;
 
 import dev.yeff.orbital.Game;
+import dev.yeff.orbital.io.Input;
 import dev.yeff.orbital.resources.ResourceManager;
 import dev.yeff.orbital.resources.Sprite;
 import dev.yeff.orbital.scenes.Scene;
@@ -11,23 +12,22 @@ import static com.raylib.Raylib.*;
 
 
 public class MainScene implements Scene {
-    private Sprite fastj;
+    private Vector2 pos;
 
     @Override
     public void init(Game game) {
         System.out.println("main scene initialized");
 
-        // forgive me, gods of programming
-        fastj = ResourceManager.getSprite("C:\\Users\\aditc\\dev\\Orbital\\examples\\src\\main\\resources\\fastj_icon.png");
+        pos = new Vector2().x(game.getWidth() / 2).y(game.getHeight() / 2);
     }
 
     @Override
     public void update(Game game, float fps) {
-        fastj = ResourceManager.getSprite("C:\\Users\\aditc\\dev\\Orbital\\examples\\src\\main\\resources\\fastj_icon.png");
+        if (Input.getKeyboard().isKeyDown(KEY_W)) pos.y(pos.y() - 5);
+        if (Input.getKeyboard().isKeyDown(KEY_S)) pos.y(pos.y() + 5);
+        if (Input.getKeyboard().isKeyDown(KEY_A)) pos.x(pos.x() - 5);
+        if (Input.getKeyboard().isKeyDown(KEY_D)) pos.x(pos.x() + 5);
 
-        game.getRenderer().drawTexture(fastj, new Vector2().x(GetScreenWidth() / 3).y(GetScreenHeight() / 4));
-        game.getRenderer().drawRect(RED, new Vector2().x(50).y(80), 70);
-        game.getRenderer().drawCircle(RED, new Vector2().x(80).y(350), 70);
-        game.getRenderer().drawLine(new Vector2().x(18).y(42), new Vector2().x(game.getWidth() - 18).y(42), BLACK);
+        game.getRenderer().drawCircle(RED, pos, 60);
     }
 }
