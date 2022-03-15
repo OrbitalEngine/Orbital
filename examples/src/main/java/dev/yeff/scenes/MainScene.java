b86c82501d553ea545546e879a8cd13e6559d21f
YeffyCodeGit <aditchakraborty2009@gmail.com>
2022-03-15 17:24:18 +0600

    implement simple shapes in renderer

--- examples/src/main/java/dev/yeff/scenes/MainScene.java
+++ examples/src/main/java/dev/yeff/scenes/MainScene.java
@@ -5,6 +5,8 @@ import dev.yeff.orbital.io.Input;
 import dev.yeff.orbital.scenes.Scene;
 import com.raylib.Raylib.Texture;
 
+import static com.raylib.Jaylib.BLACK;
+import static com.raylib.Jaylib.RED;
 import static com.raylib.Raylib.*;
 
 
@@ -21,8 +23,9 @@ public class MainScene implements Scene {
 
     @Override
     public void update(Game game, float fps) {
-        System.out.println(String.format("X: %s, Y: %s", Input.getMouse().getMousePos().x(), Input.getMouse().getMousePos().y()));
-
         game.getRenderer().drawTexture(tex, new Vector2().x(GetScreenWidth() / 3).y(GetScreenHeight() / 4));
+        game.getRenderer().drawRect(RED, new Vector2().x(50).y(80), 70);
+        game.getRenderer().drawCircle(RED, new Vector2().x(80).y(350), 70);
+        game.getRenderer().drawLine(new Vector2().x(18).y(42), new Vector2().x(game.getWidth() - 18).y(42), BLACK);
     }
 }
--- src/main/java/dev/yeff/orbital/graphics/Renderer.java
+++ src/main/java/dev/yeff/orbital/graphics/Renderer.java
@@ -10,4 +10,20 @@ public class Renderer {
     public void drawTexture(Texture texture, Vector2 pos) {
         DrawTextureV(texture, pos, RAYWHITE);
     }
+
+    public void drawCircle(Color color, Vector2 pos, float size) {
+        DrawCircleV(pos, size, color);
+    }
+
+    public void drawRect(Color color, Vector2 pos, Vector2 size) {
+        DrawRectangleV(pos, size, color);
+    }
+
+    public void drawRect(Color color, Vector2 pos, float size) {
+        DrawRectangleV(pos, new Vector2().x(size).y(size), color);
+    }
+
+    public void drawLine(Vector2 start, Vector2 end, Color color) {
+        DrawLineV(start, end, color);
+    }
 }
