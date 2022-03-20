d6c70b20c08bd991ea19dbadd9bb0b270d5b4942
YeffyCodeGit <aditchakraborty2009@gmail.com>
2022-03-20 15:46:52 +0600

    implement multiple scenes

--- examples/src/main/java/dev/yeff/scenes/MainScene.java
+++ examples/src/main/java/dev/yeff/scenes/MainScene.java
@@ -1,7 +1,6 @@
 package dev.yeff.scenes;
 
 import dev.yeff.orbital.Game;
-import dev.yeff.orbital.graphics.Colors;
 import dev.yeff.orbital.io.Input;
 import dev.yeff.orbital.io.Keys;
 import dev.yeff.orbital.resources.ResourceManager;
@@ -9,7 +8,6 @@ import dev.yeff.orbital.resources.Sprite;
 import dev.yeff.orbital.scenes.Scene;
 import dev.yeff.orbital.util.Log;
 
-import static com.raylib.Jaylib.RED;
 import static com.raylib.Raylib.*;
 
 public class MainScene implements Scene {
@@ -21,16 +19,20 @@ public class MainScene implements Scene {
         Log.info(MainScene.class, "main scene initialized");
 
         // forgive me, gods of programming
-        pos = new Vector2().x(GetScreenWidth() / 3).y(GetScreenHeight() / 4);
-        sprite = ResourceManager.getSprite("C:\\Users\\aditc\\dev\\Orbital\\examples\\src\\main\\resources\\fastj_icon.png");
+        pos = new Vector2().x(game.getWidth() / 3).y(game.getHeight() / 4);
+        sprite = ResourceManager.getSprite("C:\\Users\\aditc\\dev\\Orbital\\examples\\src\\main\\resources\\Orbital.png");
     }
 
     @Override
     public void update(Game game, float fps) {
-        if (Input.getKeyboard().isKeyDown(Keys.W))
-            game.loadScene("Other");
+        if (Input.getKeyboard().isKeyDown(Keys.W)) pos.y(pos.y() - 5);
+        if (Input.getKeyboard().isKeyDown(Keys.S)) pos.y(pos.y() + 5);
+        if (Input.getKeyboard().isKeyDown(Keys.A)) pos.x(pos.x() - 5);
+        if (Input.getKeyboard().isKeyDown(Keys.D)) pos.x(pos.x() + 5);
 
-        sprite.resize(new Vector2().x(450).y(300));
+        if (Input.getKeyboard().isKeyDown(Keys.SPACE)) game.loadScene("Other");
+
+        sprite.resize(new Vector2().x(300).y(300));
         game.getRenderer().drawTexture(sprite, pos);
     }
 
