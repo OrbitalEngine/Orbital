9972740bb7ce055e5960a9c0ec982b01b213ce0c
YeffyCodeGit <aditchakraborty2009@gmail.com>
2022-03-24 17:41:42 +0600

    add documentation for vector2f class

--- src/main/java/dev/yeff/orbital/math/Vector2f.java
+++ src/main/java/dev/yeff/orbital/math/Vector2f.java
@@ -2,6 +2,12 @@ package dev.yeff.orbital.math;
 
 import com.raylib.Raylib;
 
+/**
+ * Represents a vector with x and y floating-point components.
+ *
+ * @author YeffyCodeGit
+ * @version 0.0.1
+ */
 public class Vector2f {
     public float x;
     public float y;
@@ -21,51 +27,104 @@ public class Vector2f {
         this.y = 0;
     }
 
+    /**
+     * Adds the x and y components of another vector to the x and y components of the current instance.
+     *
+     * @param other The other vector to add with.
+     */
     public void add(Vector2f other) {
         x = x + other.x;
         y = y + other.y;
     }
 
+    /**
+     * Adds the x and y values to the x and y components of the current instance.
+     *
+     * @param x The x value to add.
+     * @param y The y value to add.
+     */
     public void add(float x, float y) {
         this.x = this.x + x;
         this.y = this.y + y;
     }
 
+    /**
+     * Subtracts the x and y components of another vector from the x and y components of the current instance.
+     *
+     * @param other The other vector to subtract from.
+     */
     public void subtract(Vector2f other) {
         x = x - other.x;
         y = y - other.y;
     }
 
+    /**
+     * Subtracts the x and y values from the x and y components of the current instance.
+     *
+     * @param x The x value to subtract.
+     * @param y The y value to subtract.
+     */
     public void subtract(float x, float y) {
         this.x = this.x - x;
         this.y = this.y - y;
     }
 
+    /**
+     * Multiplies the x and y components of another vector with the x and y components of the current instance.
+     *
+     * @param other The other vector to multiply with.
+     */
     public void multiply(Vector2f other) {
         x = this.x * other.x;
         y = this.y * other.y;
     }
 
+    /**
+     * Multiplies the x and y values with the x and y components of the current instance.
+     *
+     * @param x The x value to multiply with.
+     * @param y The y value to multiply with.
+     */
     public void multiply(float x, float y) {
         this.x = this.x * x;
         this.y = this.y * y;
     }
 
+    /**
+     * Divides the x and y components of another vector from the x and y components of the current instance.
+     *
+     * @param other The other vector to divide from.
+     */
     public void divide(Vector2f other) {
         x = x / other.x;
         y = y / other.y;
     }
 
+    /**
+     * Divides the x and y values from the x and y components of the current instance.
+     *
+     * @param x The x value to divide.
+     * @param y The y value to divide.
+     */
     public void divide(float x, float y) {
         this.x = this.x / x;
         this.y = this.y / y;
     }
 
+
+    /**
+     * Sets the x and y components of the current instance to zero.
+     */
     public void zero() {
         x = 0.0f;
         y = 0.0f;
     }
 
+    /**
+     * Returns the raylib version of the current instance vector. This function is mainly meant to be used by the engine internally.
+     *
+     * @return The raylib version of this vector.
+     */
     public Raylib.Vector2 asRaylibVector() {
         return new Raylib.Vector2().x(x).y(y);
     }
