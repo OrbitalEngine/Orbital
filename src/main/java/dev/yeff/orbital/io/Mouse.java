feffa1833eebc2be76456c03c801cfe6af70ebc0
YeffyCodeGit <aditchakraborty2009@gmail.com>
2022-03-25 10:38:24 +0600

    add documentation for mouse functions

--- src/main/java/dev/yeff/orbital/io/Mouse.java
+++ src/main/java/dev/yeff/orbital/io/Mouse.java
@@ -1,6 +1,5 @@
 package dev.yeff.orbital.io;
 
-import com.raylib.Raylib.Vector2;
 import dev.yeff.orbital.math.Vector2f;
 
 import static com.raylib.Raylib.*;
@@ -12,20 +11,47 @@ import static com.raylib.Raylib.*;
  * @version 0.0.1
  */
 public class Mouse {
+    /**
+     * Gets the position of the mouse.
+     *
+     * @return The mouse position.
+     */
     public Vector2f getMousePos() {
         return new Vector2f(GetMousePosition().x(), GetMousePosition().y());
     }
 
+    /**
+     * Gets the position of the mouse between frames.
+     *
+     * @return The delta position of the mouse.
+     */
     public Vector2f getMouseDelta() { return new Vector2f(GetMouseDelta().x(), GetMouseDelta().y()); }
 
+    /**
+     * Checks if a mouse button is being held down.
+     *
+     * @param button The button to check for.
+     * @return If the is not held down or not.
+     */
     public boolean isMouseDown(Keys button) {
         return IsMouseButtonDown(button.getKeycode());
     }
 
+    /**
+     * Gets the scroll value of the mouse.
+     *
+     * @return The scroll value.
+     */
     public float getScroll() {
         return GetMouseWheelMove() * 4;
     }
 
+    /**
+     * Checks if a mouse button is not being held down.
+     *
+     * @param button The button to check for.
+     * @return If the is not held down or not.
+     */
     public boolean isMouseUp(Keys button) {
         return IsMouseButtonUp(button.getKeycode());
     }
