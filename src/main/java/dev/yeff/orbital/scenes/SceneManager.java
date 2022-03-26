d552db29fcc49c09965bab586820f24aa98e4e7d
YeffyCodeGit <aditchakraborty2009@gmail.com>
2022-03-26 15:08:59 +0600

    added docs to scenemanager

--- src/main/java/dev/yeff/orbital/scenes/SceneManager.java
+++ src/main/java/dev/yeff/orbital/scenes/SceneManager.java
@@ -15,10 +15,22 @@ public class SceneManager {
 
     private static Map<String, Scene> scenes = new HashMap<>();
 
+    /**
+     * Adds a new scene to the scene manager.
+     *
+     * @param name The name of the scene to add it as.
+     * @param scene The scene class.
+     */
     public static void addScene(String name, Scene scene) {
         scenes.put(name, scene);
     }
 
+    /**
+     * Gets a scene from the scene manager.
+     *
+     * @param sceneName The name of the scene to get.
+     * @return The scene.
+     */
     public static Scene getScene(String sceneName) {
         return scenes.get(sceneName);
     }
