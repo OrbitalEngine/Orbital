8a025dccf0488bb633387fd6ff69c10bd7dbf25a
YeffyCodeGit <aditchakraborty2009@gmail.com>
2022-04-06 17:08:47 +0600

    add checks in all builder functions

--- src/main/java/dev/yeff/orbital/ecs/builders/GameObjectBuilder.java
+++ src/main/java/dev/yeff/orbital/ecs/builders/GameObjectBuilder.java
@@ -2,10 +2,10 @@ package dev.yeff.orbital.ecs.builders;
 
 import dev.yeff.orbital.ecs.Component;
 import dev.yeff.orbital.ecs.GameObject;
-import dev.yeff.orbital.ecs.components.RenderShapeComponent;
-import dev.yeff.orbital.ecs.components.SpriteComponent;
-import dev.yeff.orbital.ecs.components.TransformComponent;
+import dev.yeff.orbital.ecs.components.*;
 import dev.yeff.orbital.math.Vector2f;
+
+import javax.sound.sampled.Line;
 
 public class GameObjectBuilder {
     private GameObject object;
@@ -24,17 +24,39 @@ public class GameObjectBuilder {
     }
 
     public GameObjectBuilder withShape(RenderShapeComponent shape) {
-        if (!object.hasComponent(SpriteComponent.class))
+        if (!object.hasComponent(SpriteComponent.class)
+                && !object.hasComponent(LineComponent.class)
+                && !object.hasComponent(TextComponent.class))
             return withComponent(shape);
         else
-            throw new IllegalStateException("Cannot add shape component to object with sprite component");
+            throw new IllegalStateException("Cannot add shape component to object with other render component");
     }
 
     public GameObjectBuilder withSprite(SpriteComponent sprite) {
-        if (!object.hasComponent(RenderShapeComponent.class))
+        if (!object.hasComponent(RenderShapeComponent.class)
+                && !object.hasComponent(LineComponent.class)
+                && !object.hasComponent(TextComponent.class))
             return withComponent(sprite);
         else
-            throw new IllegalStateException("Cannot add sprite component to object with shape component");
+            throw new IllegalStateException("Cannot add sprite component to object with other render component");
+    }
+
+    public GameObjectBuilder withLine(LineComponent line) {
+        if (!object.hasComponent(RenderShapeComponent.class)
+                && !object.hasComponent(SpriteComponent.class)
+                && !object.hasComponent(TextComponent.class))
+            return withComponent(line);
+        else
+            throw new IllegalStateException("Cannot add line component to object with other render component");
+    }
+
+    public GameObjectBuilder withText(TextComponent line) {
+        if (!object.hasComponent(RenderShapeComponent.class)
+                && !object.hasComponent(SpriteComponent.class)
+                && !object.hasComponent(LineComponent.class))
+            return withComponent(line);
+        else
+            throw new IllegalStateException("Cannot add line component to object with other render component");
     }
 
     public GameObject build() {
