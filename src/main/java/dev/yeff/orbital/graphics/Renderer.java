e5e864f6d63291c52cb538f5fb91dc2505fed93e
YeffyCodeGit <aditchakraborty2009@gmail.com>
2022-04-08 09:53:28 +0600

    make renderer draw functions private

--- src/main/java/dev/yeff/orbital/graphics/Renderer.java
+++ src/main/java/dev/yeff/orbital/graphics/Renderer.java
@@ -86,7 +86,7 @@ public class Renderer {
      * @param sprite The sprite to draw.
      * @param pos The position to draw it at.
      */
-    public static void drawTexture(Sprite sprite, Vector2f pos) {
+    private static void drawTexture(Sprite sprite, Vector2f pos) {
         DrawTexture(sprite.getRawTex(), (int) pos.x, (int) pos.y, RAYWHITE);
     }
 
@@ -97,7 +97,7 @@ public class Renderer {
      * @param pos The position to draw the circle at.
      * @param radius The radius of the circle.
      */
-    public static void drawCircle(Colors color, Vector2f pos, float radius) {
+    private static void drawCircle(Colors color, Vector2f pos, float radius) {
         DrawCircle((int) pos.x, (int) pos.y, radius, color.getColor());
     }
 
@@ -108,7 +108,7 @@ public class Renderer {
      * @param pos The position to draw the circle outline at.
      * @param radius The radius of the circle.
      */
-    public static void drawCircleOutline(Colors color, Vector2f pos, float radius) {
+    private static void drawCircleOutline(Colors color, Vector2f pos, float radius) {
         DrawCircleLines((int) pos.x, (int) pos.y, radius, color.getColor());
     }
 
@@ -141,7 +141,7 @@ public class Renderer {
      * @param size The width and height of the rectangle.
      * @param color The color of the outline.
      */
-    public static void drawRectOutline(Vector2f pos, Vector2f size, Colors color) {
+    private static void drawRectOutline(Vector2f pos, Vector2f size, Colors color) {
         DrawRectangleLines((int) pos.x, (int) pos.y, (int) size.x, (int) size.y, color.getColor());
     }
 
@@ -152,7 +152,7 @@ public class Renderer {
      * @param size The size of the rectangle.
      * @param color The color of the outline.
      */
-    public static void drawRectOutline(Vector2f pos, float size, Colors color) {
+    private static void drawRectOutline(Vector2f pos, float size, Colors color) {
         DrawRectangleLines((int) pos.x, (int) pos.y, (int) size, (int) size, color.getColor());
     }
 
@@ -163,7 +163,7 @@ public class Renderer {
      * @param end The ending point of the line.
      * @param color The color of the line.
      */
-    public static void drawLine(Vector2f start, Vector2f end, Colors color) {
+    private static void drawLine(Vector2f start, Vector2f end, Colors color) {
         DrawLine((int) start.x, (int) start.y, (int) end.x, (int) end.y, color.getColor());
     }
 
@@ -175,7 +175,7 @@ public class Renderer {
      * @param color The color of the line.
      * @param thickness The thickness of the line.
      */
-    public static void drawLine(Vector2f start, Vector2f end, Colors color, float thickness) {
+    private static void drawLine(Vector2f start, Vector2f end, Colors color, float thickness) {
         DrawLineEx(start.asRaylibVector(), end.asRaylibVector(), thickness, color.getColor());
     }
 
@@ -186,7 +186,7 @@ public class Renderer {
      * @param fontSize The font size of the text.
      * @param pos The position to draw the text at.
      */
-    public static void drawString(String text, float fontSize, Vector2f pos) {
+    private static void drawString(String text, float fontSize, Vector2f pos) {
         DrawText(text, (int) pos.x, (int) pos.y, (int) fontSize, Colors.BLACK.getColor());
     }
 
@@ -198,7 +198,7 @@ public class Renderer {
      * @param pos The position to draw the text at.
      * @param font The font to draw the text with.
      */
-    public static void drawString(String text, float fontSize, Vector2f pos, Font font) {
+    private static void drawString(String text, float fontSize, Vector2f pos, Font font) {
         DrawTextEx(font.asRaylibFont(), text, pos.asRaylibVector(), fontSize, 2, Colors.BLACK.getColor());
     }
 
