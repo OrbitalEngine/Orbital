9f982f7940694b73b5debb9f345f30f7ccd61e38
YeffyCodeGit <aditchakraborty2009@gmail.com>
2022-04-14 11:40:32 +0600

    add docs to drawable class

--- src/main/java/dev/yeff/orbital/ecs/components/DrawableComponent.java
+++ src/main/java/dev/yeff/orbital/ecs/components/DrawableComponent.java
@@ -2,4 +2,9 @@ package dev.yeff.orbital.ecs.components;
 
 import dev.yeff.orbital.ecs.Component;
 
+/**
+ * Abstract component class which all components that implement rendering should extend. This class exists purely to make the engine code cleaner.
+ *
+ * @author YeffyCodeGit
+ */
 public abstract class DrawableComponent extends Component {  }
