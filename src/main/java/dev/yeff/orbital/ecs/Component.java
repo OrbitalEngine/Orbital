ea0b232624da2428904d27de25cde2d7e9561771
YeffyCodeGit <aditchakraborty2009@gmail.com>
2022-04-15 15:07:59 +0600

    change function order

--- src/main/java/dev/yeff/orbital/ecs/Component.java
+++ src/main/java/dev/yeff/orbital/ecs/Component.java
@@ -11,16 +11,16 @@ public abstract class Component {
     public GameObject parent = null;
 
     /**
-     * Gets called every frame, when the component updates.
-     *
-     * @param game The game instance.
-     */
-    public abstract void update(Game game);
-
-    /**
      * Gets called when the component is initialized.
      *
      * @param game The game instance.
      */
     public abstract void init(Game game);
+
+    /**
+     * Gets called every frame, when the component updates.
+     *
+     * @param game The game instance.
+     */
+    public abstract void update(Game game);
 }
