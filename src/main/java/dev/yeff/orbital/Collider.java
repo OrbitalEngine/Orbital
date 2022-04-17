1608223e1ef782db12686a7a450c48ce5e017b83
YeffyCodeGit <aditchakraborty2009@gmail.com>
2022-04-17 16:48:10 +0600

    add getRect function

--- src/main/java/dev/yeff/orbital/Collider.java
+++ src/main/java/dev/yeff/orbital/Collider.java
@@ -8,20 +8,18 @@ import dev.yeff.orbital.ecs.components.TransformComponent;
 import static com.raylib.Raylib.*;
 
 public class Collider {
-    public static boolean rectWithRect(GameObject obj1, GameObject obj2) {
-        Jaylib.Rectangle rect1 = new Jaylib.Rectangle(
-                obj1.getComponent(TransformComponent.class).position.x,
-                obj1.getComponent(TransformComponent.class).position.y,
-                obj1.getComponent(TransformComponent.class).scale.x,
-                obj1.getComponent(TransformComponent.class).scale.y
+    private static Jaylib.Rectangle getRect(GameObject obj) {
+        return new Jaylib.Rectangle(
+                obj.getComponent(TransformComponent.class).position.x,
+                obj.getComponent(TransformComponent.class).position.y,
+                obj.getComponent(TransformComponent.class).scale.x,
+                obj.getComponent(TransformComponent.class).scale.y
         );
+    }
 
-        Jaylib.Rectangle rect2 = new Jaylib.Rectangle(
-                obj2.getComponent(TransformComponent.class).position.x,
-                obj2.getComponent(TransformComponent.class).position.y,
-                obj2.getComponent(TransformComponent.class).scale.x,
-                obj2.getComponent(TransformComponent.class).scale.y
-        );
+    public static boolean rectWithRect(GameObject obj1, GameObject obj2) {
+        Jaylib.Rectangle rect1 = getRect(obj1);
+        Jaylib.Rectangle rect2 = getRect(obj2);
 
         return CheckCollisionRecs(rect1, rect2);
     }
@@ -40,12 +38,7 @@ public class Collider {
         Raylib.Vector2 center = circle.getComponent(TransformComponent.class).position.asRaylibVector();
         float radius = circle.getComponent(TransformComponent.class).scale.x;
 
-        Jaylib.Rectangle rect = new Jaylib.Rectangle(
-                rectangle.getComponent(TransformComponent.class).position.x,
-                rectangle.getComponent(TransformComponent.class).position.y,
-                rectangle.getComponent(TransformComponent.class).scale.x,
-                rectangle.getComponent(TransformComponent.class).scale.y
-        );
+        Jaylib.Rectangle rect = getRect(rectangle);
 
         return CheckCollisionCircleRec(center, radius, rect);
     }
