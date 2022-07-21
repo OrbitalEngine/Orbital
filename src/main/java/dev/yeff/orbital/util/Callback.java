7d6b1b4758d2aaa0238c55d44753c0c363597bec
YeffyCodeGit <adit.chakraboty2009@gmail.com>
2022-07-21 13:58:08 +0530

    add @FunctionalInterface annotation

--- src/main/java/dev/yeff/orbital/util/Callback.java
+++ src/main/java/dev/yeff/orbital/util/Callback.java
@@ -2,10 +2,11 @@ package dev.yeff.orbital.util;
 
 
 /**
- * Utility class to take a callback with a single parameter, of type {@code T}.
+ * Functional interface to take a callback with a single parameter, of type {@code T}.
  *
  * @author YeffyCodeGit
  */
+@FunctionalInterface
 public interface Callback<T> {
     void invoke(T param);
 }
